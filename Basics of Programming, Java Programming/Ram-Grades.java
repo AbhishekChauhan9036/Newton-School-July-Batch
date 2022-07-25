1509769c48f12ba2da6186771d88eedafe1a1cb2
Abhi <abhishekchauhan9036@gmail.com>
2022-07-25 19:28:12 +0530

    commit message

--- Basics of Programming, Java Programming/Ram-Grades.java
+++ Basics of Programming, Java Programming/Ram-Grades.java
@@ -12,11 +12,11 @@ import java.io.*;
 class Main {
 	public static void main (String[] args) {
         Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
+        int a=sc.nextInt();
         int b=sc.nextInt();
         int c=sc.nextInt();
         int d=sc.nextInt();
-		System.out.println(((a+b+c+d)/4));
+        System.out.println(((a+b+c+d)/4));
 	}
 }
 /******************************************************************************************************************************************/
