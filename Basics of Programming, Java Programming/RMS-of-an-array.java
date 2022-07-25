bc139747ab34360471d59038ee5104e01b735e16
Abhi <abhishekchauhan9036@gmail.com>
2022-07-25 19:25:37 +0530

    commit message

---
+++ Basics of Programming, Java Programming/RMS-of-an-array.java
@@ -0,0 +1,40 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static double Solve(int arr[],int n){
+		int s=0;double m=0;double r=0;
+
+		for(int i=0;i<n;i++){
+			s+=Math.pow(arr[i],2);
+		}
+
+		m=(s/(double)(n));
+		r=(double)Math.sqrt(m);
+
+		return r;
+	}
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+
+		int arr[]=new int[n];
+
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+
+		double ans=(Solve(arr,n));
+		System.out.printf("%.6f",ans);
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
