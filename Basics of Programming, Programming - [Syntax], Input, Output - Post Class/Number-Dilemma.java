7726ea7f62eaf3abbaedf6987d1925ff03088178
Abhi <abhishekchauhan9036@gmail.com>
2022-07-26 05:49:54 +0530

    commit message

---
+++ Basics of Programming, Programming - [Syntax], Input, Output - Post Class/Number-Dilemma.java
@@ -0,0 +1,19 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+static int StringToInt(String S){
+return Integer.parseInt(S);
+}
+
+static String IntToString(int N){
+return Integer.toString(N);
+}
+
+/******************************************************************************************************************************************/
+/***********End****************/
---
+++ Basics of Programming, Programming - [Syntax], Input, Output - Post Class/String-Typecast.java
@@ -0,0 +1,17 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+
+static String checkConevrtion(int a)
+{
+    return String.valueOf(a);
+}
+
+/******************************************************************************************************************************************/
+/***********End****************/
