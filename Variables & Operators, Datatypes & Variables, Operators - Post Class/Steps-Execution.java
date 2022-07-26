632e540b51eefb744f9fa16f5b62c45e56fce218
Abhi <abhishekchauhan9036@gmail.com>
2022-07-27 00:33:01 +0530

    commit message

---
+++ Variables & Operators, Datatypes & Variables, Operators - Post Class/Steps-Execution.java
@@ -0,0 +1,20 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+    public static void main (String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        System.out.print((((((a)+8)/3)%5)*5));  
+    }
+}
+/******************************************************************************************************************************************/
+/***********End****************/
