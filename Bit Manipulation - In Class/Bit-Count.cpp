d4e2614cfc867486ee32bc4ff4db4d0ec26f012b
Abhi <abhishekchauhan9036@gmail.com>
2022-08-13 06:16:38 +0530

    commit message

---
+++ Bit Manipulation - In Class/Bit-Count.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+int countSetBits(int n)
+{
+	int count = 0;
+	while (n) {
+		count += n & 1;
+		n >>= 1;
+	}
+	return count;
+}
+
+
+int32_t main()
+{
+	int n;
+	cin>>n;
+	cout << countSetBits(n);
+	return 0;
+}
