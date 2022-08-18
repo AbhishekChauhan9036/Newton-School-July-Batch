66e4bd92dc6ea4a879f70b73a82558d6709f40e7
Abhi <abhishekchauhan9036@gmail.com>
2022-08-18 06:46:49 +0530

    commit message

--- 1-D Array, 2-D Arrays, Maths. prime/Simple-transpose.java
+++ 1-D Array, 2-D Arrays, Maths. prime/Simple-transpose.java
@@ -1,4 +1,4 @@
-import java.io.*; // for handling input/output
+import java.io.*; // for handling input/output..
 import java.util.*; // contains Collections framework
 
 // don't change the name of this class
